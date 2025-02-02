9a3f87a0a036af6a7474e29dfea96f1076acf410
Alwi842 <alwi.abdullah1221@gmail.com>
2025-02-02 19:03:51 +0700

    eslint bugfix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import babelParser from "@babel/eslint-parser"; // import babel parser
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+
+  // Add your custom parser and parserOptions here
+  {
+    parser: babelParser, // specify the babel parser
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+    },
+    rules: {
+      // You can also add custom ESLint rules here
+    },
+  },
+];
 
 export default eslintConfig;
